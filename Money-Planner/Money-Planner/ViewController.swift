c3a247cd74e9422cda5bc3c45971f223a6b1cb65
2021056317 <myday2day@hanyang.ac.kr>
2024-01-05 17:52:49 +0900

    [add] xcode project init

---
+++ Money-Planner/Money-Planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money-Planner
+//
+//  Created by 유철민 on 1/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
