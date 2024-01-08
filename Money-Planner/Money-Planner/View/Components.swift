9751bc82951e31164db282a8ac4ed13eb3c53f58
2021056317 <myday2day@hanyang.ac.kr>
2024-01-08 15:54:11 +0900

    [FEAT] Design Components
    https://www.notion.so/Design-Component-2fd77d868b924b1a99c5ca09adcd46ac?pvs=4

--- Money-Planner/Money-Planner/View/Components.swift
+++ Money-Planner/Money-Planner/View/Components.swift
@@ -8,11 +8,101 @@
 import Foundation
 import UIKit
 
+// Fonts
+extension UIFont {
+    // 각 폰트 스타일에 대한 메소드 정의
+    static func mpFont26B() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: 26) ?? UIFont.systemFont(ofSize: 26, weight: .bold)
+    }
+    
+    static func mpFont20B() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .bold)
+    }
+    
+    static func mpFont20M() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Medium", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .medium)
+    }
+    
+    static func mpFont20R() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Regular", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .regular)
+    }
+    
+    static func mpFont18B() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .bold)
+    }
+    
+    static func mpFont18M() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Medium", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .medium)
+    }
+    
+    static func mpFont18R() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Regular", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .regular)
+    }
+    
+    static func mpFont16B() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .bold)
+    }
+    
+    static func mpFont16M() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Medium", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .medium)
+    }
+    
+    static func mpFont16R() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Regular", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .regular)
+    }
+    
+    static func mpFont14B() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .bold)
+    }
+    
+    static func mpFont14M() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Medium", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .medium)
+    }
+    
+    static func mpFont14R() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Regular", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .regular)
+    }
+    
+    static func mpFont12M() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Regular", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: .medium)
+    }
+    
+    static func mpFont12R() -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Regular", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: .regular)
+    }
+}
+
+
+
+// CustomLabel (letter spacing -2%)
+class MPLabel: UILabel {
+    override var text: String? {
+        didSet {
+            updateTextSpacing()
+        }
+    }
+    
+    // 문자 간격 업데이트 메소드
+    private func updateTextSpacing() {
+        guard let text = self.text else { return }
+        let attributedString = NSMutableAttributedString(string: text)
+        let letterSpacing = -0.02 * self.font.pointSize // 글자 크기의 -2%
+        attributedString.addAttribute(.kern, value: letterSpacing, range: NSRange(location: 0, length: text.count))
+        self.attributedText = attributedString
+    }
+    
+    /// 사용방법
+    /// let label = MPLabel()
+    /// label.font = UIFont.systemFont(ofSize: 16)
+    /// label.text = "커스텀 레이블 테스트"
+}
+
+
 // HeaderView
 class HeaderView: UIView {
     
     private let backButton = UIButton()
-    private let titleLabel = UILabel()
+    private let titleLabel = MPLabel()
     
     init(title: String, frame: CGRect = .zero) {
         super.init(frame: frame)
@@ -43,7 +133,7 @@ class HeaderView: UIView {
     //받은 제목에 맞춰서 title 수정
     private func setupTitleLabel(with title: String) {
         titleLabel.text = title
-        titleLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        titleLabel.font = UIFont.mpFont20B()
         titleLabel.textAlignment = .center
         addSubview(titleLabel)
     }
@@ -66,13 +156,13 @@ class HeaderView: UIView {
 
 
 //DescriptionView
-class DescriptionView: UILabel {
+class DescriptionView: MPLabel {
     // 초기화 메소드
     init(text: String, alignToCenter: Bool) {
         super.init(frame: .zero)
         self.text = text
         self.textAlignment = alignToCenter ? .center : .left
-        self.font = UIFont.systemFont(ofSize: 22, weight: .bold)  // 크고 진한 글자
+        self.font = UIFont.mpFont26B()
         self.numberOfLines = 0  // 여러 줄 표시 가능
     }
     
@@ -84,14 +174,14 @@ class DescriptionView: UILabel {
 
 
 //SubDescriptionView
-class SubDescriptionView: UILabel {
+class SubDescriptionView: MPLabel {
     // 초기화 메소드
     init(text: String, alignToCenter: Bool) {
         super.init(frame: .zero)
         self.text = text
         self.textAlignment = alignToCenter ? .center : .left
         self.textColor = UIColor.mpGray
-        self.font = UIFont.systemFont(ofSize: 16, weight: .regular)  // 보통 크기와 두께의 글자
+        self.font = UIFont.mpFont16B() 
         self.numberOfLines = 0  // 여러 줄 표시 가능
     }
     
@@ -100,8 +190,46 @@ class SubDescriptionView: UILabel {
     }
 }
 
-//BottomButton
-class BottomButton: UIButton {
+//check
+class CheckBtn: UIButton {
+    // 체크 상태 프로퍼티
+    var isChecked: Bool = true {
+        didSet {
+            updateImage()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addTarget(self, action: #selector(toggleCheck), for: .touchUpInside)
+        updateImage()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // 체크 상태 토글 메소드
+    @objc private func toggleCheck() {
+        isChecked = !isChecked
+    }
+    
+    // 이미지 업데이트 메소드
+    private func updateImage() {
+        let systemName = "checkmark.circle.fill"
+        let color = isChecked ? UIColor.mpMain : UIColor.mpGray
+        setImage(UIImage(systemName: systemName)?.withTintColor(color, renderingMode: .alwaysOriginal), for: .normal)
+    }
+    
+    // 초기 상태 설정 메소드
+    func setChecked(_ checked: Bool) {
+        isChecked = checked
+    }
+}
+
+
+//MainBottomButton
+class MainBottomBtn: UIButton {
     // 초기화 메소드
     init(title: String, frame: CGRect = .zero) {
         super.init(frame: frame)
@@ -111,13 +239,106 @@ class BottomButton: UIButton {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    override var isEnabled: Bool {
+        didSet {
+            updateButtonStyle()
+        }
+    }
+    
     private func setupButton(title: String) {
         setTitle(title, for: .normal)
-        backgroundColor = .darkGray  // 어두운 배경색 설정
-        setTitleColor(.white, for: .normal)  // 흰색 글씨색 설정
         layer.cornerRadius = 10  // 둥근 모서리 설정
         titleLabel?.font = UIFont.systemFont(ofSize: 16)  // 폰트 크기 설정
+        updateButtonStyle()
+    }
+    
+    // 버튼 스타일 업데이트 메소드
+    private func updateButtonStyle() {
+        if isEnabled {
+            backgroundColor = .mpMain
+            setTitleColor(.mpWhite, for: .normal)
+        } else {
+            backgroundColor = .mpLightGray
+            setTitleColor(.mpGray, for: .normal)
+        }
     }
 }
 
+
+//smallButton
+class SmallBtnView: UIView {
+    // 버튼 생성
+    private let cancelButton = UIButton()
+    private let completeButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    ///사용하는 viewcontroller에서 viewdidload에서 반드시 넣어줄것!
+    ///smallButtonView.addCancelAction(target: self, action: #selector(cancelButtonTapped))
+    ///smallButtonView.addCompleteAction(target: self, action: #selector(completeButtonTapped))
+    ///
+    ///@objc func cancelButtonTapped() {
+    ///print("취소 버튼이 탭되었습니다.")
+    /// 취소 버튼 액션 처리
+    ///}
+    ///
+    // 취소 버튼에 액션 추가 메소드
+    func addCancelAction(target: Any?, action: Selector) {
+        cancelButton.addTarget(target, action: action, for: .touchUpInside)
+    }
+    
+    // 완료 버튼에 액션 추가 메소드
+    func addCompleteAction(target: Any?, action: Selector) {
+        completeButton.addTarget(target, action: action, for: .touchUpInside)
+    }
+    
+    private func setupButtons() {
+        // 취소 버튼 설정
+        cancelButton.setTitle("취소", for: .normal)
+        cancelButton.setTitleColor(UIColor(named:"mpMainColor"), for: .normal)
+        cancelButton.layer.borderColor = UIColor(named: "mpMainColor")?.cgColor
+        cancelButton.layer.borderWidth = 1
+        cancelButton.backgroundColor = UIColor.mpWhite
+        cancelButton.layer.cornerRadius = 10  // 적절한 둥글기 값 설정
+        cancelButton.clipsToBounds = true
+        
+        // 완료 버튼 설정
+        completeButton.setTitle("완료", for: .normal)
+        completeButton.setTitleColor(UIColor.mpWhite, for: .normal)
+        completeButton.backgroundColor = UIColor(named: "mpMainColor")
+        completeButton.layer.cornerRadius = 10  // 적절한 둥글기 값 설정
+        completeButton.clipsToBounds = true
+        
+        // 버튼을 뷰에 추가
+        addSubview(cancelButton)
+        addSubview(completeButton)
+        
+        // Auto Layout 설정
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        completeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // 취소 버튼 제약 조건
+            cancelButton.leadingAnchor.constraint(equalTo: leadingAnchor),
+            cancelButton.topAnchor.constraint(equalTo: topAnchor),
+            cancelButton.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            // 완료 버튼 제약 조건
+            completeButton.leadingAnchor.constraint(equalTo: cancelButton.trailingAnchor, constant: 10), // 버튼 사이의 간격
+            completeButton.trailingAnchor.constraint(equalTo: trailingAnchor),
+            completeButton.topAnchor.constraint(equalTo: topAnchor),
+            completeButton.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            // 두 버튼의 너비를 동일하게 설정
+            cancelButton.widthAnchor.constraint(equalTo: completeButton.widthAnchor)
+        ])
+    }
+}
