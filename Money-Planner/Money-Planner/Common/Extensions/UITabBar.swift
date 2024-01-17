ab25c70580e92c660f5ec936d65f63913b172e7a
seonwoonam <potatomoney716@gmail.com>
2024-01-17 23:32:44 +0900

    [FEAT] : 탭바 추가

---
+++ Money-Planner/Money-Planner/Common/Extensions/UITabBar.swift
@@ -0,0 +1,18 @@
+//
+//  UITabBar.swift
+//  Money-Planner
+//
+//  Created by seonwoo on 2024/01/17.
+//
+
+import Foundation
+import UIKit
+
+extension UITabBar {
+    // 기본 그림자 스타일을 초기화해야 커스텀 스타일을 적용할 수 있다.
+    static func clearShadow() {
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().backgroundColor = UIColor.white
+    }
+}
--- Money-Planner/Money-Planner/View/Home/HomeViewController.swift
+++ Money-Planner/Money-Planner/View/Home/HomeViewController.swift
@@ -9,3 +9,9 @@ import Foundation
 import UIKit
 
 
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.mpWhite
+    }
+}
