9f3167aba1b8781973209b59aafc66c9c3c10f5b
seonwoonam <potatomoney716@gmail.com>
2024-01-22 03:10:39 +0900

    [FEAT] : 카테고리 버튼 추가

---
+++ Money-Planner/Money-Planner/CustomView/Button/CategoryButton.swift
@@ -0,0 +1,79 @@
+//
+//  CategoryButton.swift
+//  Money-Planner
+//
+//  Created by seonwoo on 2024/01/21.
+//
+
+import Foundation
+import UIKit
+
+class CategoryButton: UIView {
+    
+    var category : Category = Category(id: -1, name: "테스트"){
+        didSet{
+            textLabel.text = category.name
+        }
+    }
+    
+    var textLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.textColor = UIColor.mpWhite
+        label.textAlignment = .center
+        label.font = UIFont.mpFont14B()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let circleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.mpWhite
+        view.layer.cornerRadius = 6.5 // 동그라미의 반지름 설정
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupUI()
+    }
+    
+    func setupUI() {
+        
+        let stackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .horizontal
+            stackView.spacing = 8
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            return stackView
+        }()
+        
+        textLabel.text = category.name
+        // UIView의 기본적인 설정
+        backgroundColor = .mpMainColor
+        layer.cornerRadius = 18
+
+        stackView.addArrangedSubview(circleView)
+        stackView.addArrangedSubview(textLabel)
+        
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 37),
+            
+            // StackView의 제약 설정
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            
+            circleView.heightAnchor.constraint(equalToConstant: 13),
+            circleView.widthAnchor.constraint(equalToConstant: 13),
+        ])
+    }
+}
---
+++ Money-Planner/Money-Planner/Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Money-Planner
+//
+//  Created by seonwoo on 2024/01/21.
+//
+
+import Foundation
+
+// MARK: - Category
+struct Category: Codable {
+    let id: Int
+    let name: String
+    var categoryBudget: Int? = 0
+}
