d49d4b9d741a33c9c4d7df86010010e229d7487f
jini-coding <96573160+jini-coding@users.noreply.github.com>
2024-02-16 05:28:23 +0900

    [FEAT] : 일일소비내역 조회 api 연동

--- Money-Planner/Money-Planner/Network/API/Expense/ExpenseAPI.swift
+++ Money-Planner/Money-Planner/Network/API/Expense/ExpenseAPI.swift
@@ -5,6 +5,7 @@ import Moya
 
 enum ExpenseAPI  {
     case getSearchExpense(title : String, order : String?, size: Int?)
+    case getDailyConsumeHistory(date: String, size: Int?, lastExpenseId: Int?)
     
 }
 
@@ -28,13 +29,23 @@ extension ExpenseAPI : BaseAPI {
             }
             
             return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+            
+        case .getDailyConsumeHistory(let date, let size, let lastExpenseId):
+            var parameters: [String: Any] = ["date": date]
+            if let size = size {
+                parameters["size"] = size
+            }
+            if let lastExpenseId = lastExpenseId {
+                parameters["lastExpenseId"] = lastExpenseId
+            }
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
         }
     }
     
     public var method: Moya.Method {
         // 파라미터값을 통신요청 타입을 제작
         switch self {
-        case .getSearchExpense:
+        case .getSearchExpense, .getDailyConsumeHistory:
             return .get
         }
     }
@@ -43,11 +54,13 @@ extension ExpenseAPI : BaseAPI {
         switch self {
         case .getSearchExpense:
             return "/api/expense/search"
+        case .getDailyConsumeHistory:
+            return "/api/expense/daily"
         }
     }
     
     public var headers: [String: String]? {
-        return ["Authorization": "Bearer "] // 억세스토큰
+        return ["Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMjkwMTA2OTM0IiwiYXV0aCI6IlVTRVIiLCJleHAiOjE3MDgwNzQ1NTJ9.y1-7jbsain9TEDcLDN0A3daNvtK0xUwRqM1-ASvJARU"] // 억세스토큰
         // 실제 사용하는 헤더로 변경해야 합니다.
     }
 }
